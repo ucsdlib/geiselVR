c1dde75c2c6975dfe3134694311d270a4d87c621
Mcavoy <smcavoy@UCSD.EDU>
2017-04-25 16:01:30 -0700

    0.1.2 Translate on button press

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,13 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections;
+using Debug = System.Diagnostics.Debug;
 
-public class MoveSCene : MonoBehaviour {
+public class Mover : MonoBehaviour
+{
+
+    private Vector3 startTransform;
 
 	void Start () {
-	
+	    startTransform = gameObject.transform.position;
 	}
 
 	void Update () {
-	
+	    // Handle 'A' button press
+	    if (OVRInput.GetDown(OVRInput.Button.One))
+	    {
+	        gameObject.transform.Translate(0, 0, 0.5f);
+	    }
+
+	    // Handle trigger press
+	    var flex = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+	    if (flex > 0.5)
+	    {
+	        gameObject.transform.position = startTransform;
+	    }
 	}
 }
