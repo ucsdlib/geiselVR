9cb5ee807ef96553cee5cefb3ab0c96d7f5c6dca
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-05-04 17:56:59 -0700

    0.1.3 Create Full Bookshelf prefab

--- Assets/Scripts/RowController.cs
+++ Assets/Scripts/RowController.cs
@@ -1,15 +1,51 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Net.Sockets;
+using UnityEngine;
 
-public class RowController : MonoBehaviour {
+public class RowController : MonoBehaviour
+{
+	public Transform Bookshelf;
 
-	// Use this for initialization
-	void Start () {
-	
+
+	private void Start () {
+		CalculateLocalBounds();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private void Update()
+	{
+		InstantiateOnPress();
+	}
+
+	private void InstantiateOnPress()
+	{
+		// Check use pressed button
+		if (OVRInput.GetDown(OVRInput.Button.One))
+		{
+			Instantiate(Bookshelf, new Vector3(0, 0, -2),
+				Quaternion.Euler(0, 180f, 0), transform);
+		}
+	}
+
+	private void CalculateLocalBounds()
+	{
+		// Capture the initial rotation and reset to 0
+		Quaternion currentRotation = transform.rotation;
+		transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+		// Create new empty bounds
+		Bounds bounds = new Bounds(transform.position, Vector3.zero);
+
+		// Grow bounds based on all children
+		foreach (var childRenderer in GetComponents<MeshRenderer>())
+		{
+			bounds.Encapsulate(childRenderer.bounds);
+		}
+
+		// Reset center
+		Vector3 localCenter = bounds.center - transform.position;
+		bounds.center = localCenter;
+
+		Debug.Log("Local bounds: " + bounds);
+
+		transform.rotation = currentRotation;
 	}
 }
