8b26c389e02d3ca358617f4c8cc08e042589ed2e
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-05-07 19:18:34 -0700

    0.1.3 Capture initial positions in row controller

--- Assets/Scripts/RowController.cs
+++ Assets/Scripts/RowController.cs
@@ -7,15 +7,22 @@ public class RowController : MonoBehaviour
 	public float Width = 1.2f; // TODO find programatically
 	public int RowSize = 2;
 
-	private List<Transform> _activeUnits = new List<Transform>();
-	private bool _lerping;
-	private Vector3 _lerpDestPos;
+	private readonly List<Transform> _activeUnits = new List<Transform>();
+	private bool _lerping = false;
+	private Vector3? _lerpDestPos = null;
+	private Vector3 firstPos;				// first position in array
+	private Vector3 lastPos;				// last position in array
+	private Vector3 rowPos;			// initial position of row in world
 
 
 	private void Start ()
 	{
 		InstantiateArray(RowSize);
-		Debug.Log("Count: " + _activeUnits.Count);
+
+		// Save initial positions
+		firstPos = Unit.position;
+		lastPos = firstPos + Vector3.left * RowSize * Width;
+		rowPos = transform.position;
 	}
 
 	private void Update()
@@ -36,20 +43,23 @@ public class RowController : MonoBehaviour
 	private void HandleShiftInput()
 	{
 		// Get both hand x-axis thumbstick value [-1, 1]
-		float flexL = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick)[0];
+		// TODO to have both, you need a flag that sets which hand you are using at the start
+//		float flexL = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick)[0];
 		float flexR = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick)[0];
 
-		if (flexL > 0.5 || flexR > 0.5)
+		if (flexR > 0.5)
 		{
 			// Capture destination position
 			if (!_lerping)
 			{
+
 			}
 		}
 	}
 
 	private void InstantiateArray(int size)
 	{
+		// Instantiate units in correct position and add to _activeList
 		for (int i = 1; i <= size; i++)
 		{
 			Vector3 position = Unit.position + Vector3.left * i * Width;
