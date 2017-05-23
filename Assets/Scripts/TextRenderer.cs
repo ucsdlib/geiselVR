6a518e129e942d9248048341dacfbbff882a20e4
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-05-23 16:56:50 -0700

    0.2.1 Attack text to book

--- Assets/Scripts/TextRenderer.cs
+++ Assets/Scripts/TextRenderer.cs
@@ -6,75 +6,91 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 
 
-public class TextRenderer : MonoBehaviour {
+public class TextRenderer : MonoBehaviour
+{
+    public float Scale;
 
-	void Start () {
-		// Settings
-		TextGenerationSettings settings = new TextGenerationSettings();
-		settings.textAnchor = TextAnchor.MiddleCenter;
-		settings.color = Color.black;
-		settings.generationExtents = new Vector2(10, 10);
-		settings.pivot = new Vector2(0.5f, 0.5f);
-		settings.richText = true;
-		settings.font = Font.CreateDynamicFontFromOSFont("Arial", 72);
-		settings.fontSize = 24;
-		settings.fontStyle = FontStyle.Normal;
-		settings.verticalOverflow = VerticalWrapMode.Overflow;
-		settings.horizontalOverflow = HorizontalWrapMode.Overflow;
-		settings.lineSpacing = 1;
-		settings.generateOutOfBounds = true;
-		settings.resizeTextForBestFit = true;
-		settings.scaleFactor = 1f;
+    void Start()
+    {
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        gameObject.transform.localScale = Vector3.one * Scale / 10000f;
+        GenerateText("HELLO WORLD");
+    }
 
-		MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
-		MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
+    public void GenerateText(string text)
+    {
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
 
-		TextGenerator generator = new TextGenerator();
-		generator.Populate("HELLO WORLD", settings);
-		Debug.Log("Generator made " + generator.vertexCount + " vertices.");
+        TextGenerationSettings settings = GetDefSettings();
+        TextGenerator generator = new TextGenerator();
+        generator.Populate(text, settings);
+        Debug.Log("Generator made " + generator.vertexCount + " vertices."); // DEBUG
 
-		Mesh mesh = new Mesh();
-		GetMesh(generator, mesh);
-		meshFilter.mesh = mesh;
-		renderer.material = settings.font.material;
-	}
-	
-	public void GetMesh(TextGenerator generator, Mesh mesh)
-	{
-		if (mesh == null) return;
+        Mesh mesh = new Mesh();
+        GetMesh(generator, mesh);
+        meshFilter.mesh = mesh;
+        meshRenderer.material = settings.font.material;
+    }
 
-		// Copy vertex data into mesh
-		int vertSize = generator.vertexCount;
-		Vector3[] tempVerts = new Vector3[vertSize];
-		Color32[] tempColors = new Color32[vertSize];
-		Vector2[] tempUvs = new Vector2[vertSize];
-		IList<UIVertex> generatorVerts = generator.verts;
-		for (int i = 0; i < vertSize; i++)
-		{
-			tempVerts[i] = generatorVerts[i].position;
-			tempColors[i] = generatorVerts[i].color;
-			tempUvs[i] = generatorVerts[i].uv0;
-		}
-		mesh.vertices = tempVerts;
-		mesh.colors32 = tempColors;
-		mesh.uv = tempUvs;
 
-		// Map triangles to vertex data
-		int charCount = vertSize / 4;
-		int [] tempIndices = new int[charCount * 6];
-		for (int i = 0; i < charCount; i++)
-		{
-			int vertIndexStart = i * 4;
-			int trianglesIndexStart = i * 6;
-			tempIndices[trianglesIndexStart++] = vertIndexStart;
-			tempIndices[trianglesIndexStart++] = vertIndexStart + 1;
-			tempIndices[trianglesIndexStart++] = vertIndexStart + 2;
-			tempIndices[trianglesIndexStart++] = vertIndexStart;
-			tempIndices[trianglesIndexStart++] = vertIndexStart + 2;
-			tempIndices[trianglesIndexStart] = vertIndexStart + 3;
-		}
-		mesh.triangles = tempIndices;
-		mesh.RecalculateBounds();
-	}
-	
+    private TextGenerationSettings GetDefSettings()
+    {
+        return new TextGenerationSettings
+        {
+            textAnchor = TextAnchor.MiddleCenter,
+            color = Color.black,
+            generationExtents = new Vector2(10, 10),
+            pivot = new Vector2(0f, 0f),
+            richText = true,
+            font = Font.CreateDynamicFontFromOSFont("Arial", 72),
+            fontSize = 24,
+            fontStyle = FontStyle.Normal,
+            verticalOverflow = VerticalWrapMode.Overflow,
+            horizontalOverflow = HorizontalWrapMode.Overflow,
+            lineSpacing = 1,
+            generateOutOfBounds = true,
+            resizeTextForBestFit = true,
+            scaleFactor = 1f
+        };
+    }
+
+    private void GetMesh(TextGenerator generator, Mesh mesh)
+    {
+        if (mesh == null) return;
+
+        // Copy vertex data into mesh
+        int vertSize = generator.vertexCount;
+        Vector3[] tempVerts = new Vector3[vertSize];
+        Color32[] tempColors = new Color32[vertSize];
+        Vector2[] tempUvs = new Vector2[vertSize];
+        IList<UIVertex> generatorVerts = generator.verts;
+        for (int i = 0; i < vertSize; i++)
+        {
+            tempVerts[i] = generatorVerts[i].position;
+            tempColors[i] = generatorVerts[i].color;
+            tempUvs[i] = generatorVerts[i].uv0;
+        }
+        mesh.vertices = tempVerts;
+        mesh.colors32 = tempColors;
+        mesh.uv = tempUvs;
+
+        // Map triangles to vertex data
+        int charCount = vertSize / 4;
+        int[] tempIndices = new int[charCount * 6];
+        for (int i = 0; i < charCount; i++)
+        {
+            int vertIndexStart = i * 4;
+            int trianglesIndexStart = i * 6;
+            tempIndices[trianglesIndexStart++] = vertIndexStart;
+            tempIndices[trianglesIndexStart++] = vertIndexStart + 1;
+            tempIndices[trianglesIndexStart++] = vertIndexStart + 2;
+            tempIndices[trianglesIndexStart++] = vertIndexStart;
+            tempIndices[trianglesIndexStart++] = vertIndexStart + 2;
+            tempIndices[trianglesIndexStart] = vertIndexStart + 3;
+        }
+        mesh.triangles = tempIndices;
+        mesh.RecalculateBounds();
+    }
 }
