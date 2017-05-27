1a268977340212fecf9c07dd460a6ace37da8e6c
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-05-27 11:03:30 -0700

    0.2.2 Add controller to bookshelf
    Interfaces with Unit

---
+++ Assets/Scripts/BookshelfController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookshelfController : MonoBehaviour {
+    
+    private void Awake()
+    {
+        Unit unit = GetComponent<Unit>();
+        
+        if (unit != null)
+        {
+            unit.UpdateContentsDelegate += LoadBooks;
+        }
+    }
+
+    private void LoadBooks(Unit unit, bool right)
+    {
+        Debug.Log("Load Books called"); // DEBUG
+    }
+}
