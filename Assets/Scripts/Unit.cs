eb9d6ce33201f7dfcdfc8468e427b5c7ded3dbbf
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-05-27 10:46:10 -0700

    0.2.2 Add delegate to Unit

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour {
+public class Unit : MonoBehaviour
+{
+    public RowController Row { get; set; }
+    public int Index { get; set; }
 
-	public RowController Row;
-	public int Index { get; set; }
-	public DataPosition Position;
-	
+    public delegate void UpdateContents(Unit unit, bool right);
+
+    public UpdateContents UnitUpdateContents;
 }
