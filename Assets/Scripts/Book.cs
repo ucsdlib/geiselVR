a9d86b72e6c43832815b0426849d17a69310440a
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-05-27 16:55:03 -0700

    0.2.2 Add Book Prefab

---
+++ Assets/Scripts/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    
+    private int _callNumber;
+    
+    public void LoadData(int callNumber)
+    {
+        _callNumber = callNumber;
+        // TODO render text
+    }
+}
