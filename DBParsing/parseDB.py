48005d22caacc4103abdabaf3cb5dfe1e4e2289f
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-06-10 17:38:13 -0700

    0.2.4 Test sorting by call number

--- DBParsing/parseDB.py
+++ DBParsing/parseDB.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import sqlite3
 
 # Positions in CSV file
 ITEM_NUM = 0
@@ -28,16 +29,31 @@ def process_line(line):
     if (len(line) != SIZE):
         return None
 
+    return (line[CALL_NUM], line[TITLE])
 
 
-
+def store_data(data: tuple, db: sqlite3.Cursor):
+    db.execute('INSERT INTO main VALUES (?,?)', data)
 
 def main(argv):
     with open(argv[1], newline='') as csvfile:
         csvfile.readline()  # skip header
         reader = csv.reader(csvfile, delimiter='\t')
+        conn = sqlite3.connect('call-only.db')
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM main')
+
+
+        # Populate dat
+        count = 0
         for row in reader:
-            process_line(row)
+            dataTuple = process_line(row)
+            store_data(dataTuple, cursor)
+            if (count % 100000 == 0):
+                print(count)
+            count += 1
+        conn.commit()
+        conn.close()
 
 
 if __name__ == '__main__':
