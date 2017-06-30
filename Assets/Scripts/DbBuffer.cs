4e61839646d21f125dd22fb6c7ce92dece69b19e
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-06-30 14:43:08 -0700

    0.2.5 Simplify buffer and enforce direction at instantiation

--- Assets/Scripts/DbBuffer.cs
+++ Assets/Scripts/DbBuffer.cs
@@ -11,10 +11,10 @@ public class DbBuffer
         get { return _forward; }
     }
 
-    private DbWrapper _db;
+    private readonly DbWrapper _db;
+    private readonly int _capacity;
+    private readonly bool _forward;
     private string _startCallNum;
-    private int _capacity;
-    private bool _forward;
     private int _index;
     private List<DataEntry> _buffer;
 
@@ -28,47 +28,23 @@ public class DbBuffer
         _buffer = new List<DataEntry>(capacity);
     }
 
-    private bool LoadNextData()
+    private bool LoadData()
     {
         var initCount = _buffer.Count;
-        _db.QueryCount(ref _buffer, _startCallNum, _capacity,
-            startInclusive: false, forward: true);
-        return _buffer.Count > initCount; // check if we added data
-    }
-
-    private bool LoadPrevData()
-    {
-        var initCount = _buffer.Count;
-        _db.QueryCount(ref _buffer, _startCallNum, _capacity,
-            startInclusive: false, forward: false);
+        _db.QueryCount(ref _buffer, _startCallNum, _capacity, forward: _forward);
         return _buffer.Count > initCount; // check if we added data
     }
     
     [CanBeNull]
     private DataEntry NextEntry()
     {
-        if (_buffer.Count == 0 && !LoadNextData()) return null;
+        if (_buffer.Count == 0 && !LoadData()) return null;
 
         // load next round of data if needed
         if (_index >= _buffer.Count)
         {
             _startCallNum = _buffer[_buffer.Count - 1].CallNum;
-            if (!LoadNextData()) return null;
-        }
-
-        return _buffer[_index++];
-    }
-    
-    [CanBeNull]
-    private DataEntry PrevEntry()
-    {
-        if (_buffer.Count == 0 && !LoadNextData()) return null;
-
-        // load next round of data if needed
-        if (_index >= _buffer.Count)
-        {
-            _startCallNum = _buffer[_buffer.Count - 1].CallNum;
-            if (!LoadNextData()) return null;
+            if (!LoadData()) return null;
         }
 
         return _buffer[_index++];
