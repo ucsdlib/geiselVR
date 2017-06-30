241493fce41a471455206a732d3f951af1bd5a5e
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-06-30 14:00:29 -0700

    0.2.5 Implement next buffer functionality

--- Assets/Scripts/DbBuffer.cs
+++ Assets/Scripts/DbBuffer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 
 public class DbBuffer
 {
@@ -8,7 +9,7 @@ public class DbBuffer
     private string _startCallNum;
     private int _capacity;
     private int _index;
-    private List<DataEntry> _bookData;
+    private List<DataEntry> _buffer;
 
     public DbBuffer(string startCallNum, int capacity)
     {
@@ -16,27 +17,28 @@ public class DbBuffer
         _startCallNum = startCallNum;
         _capacity = capacity;
         _index = 0;
-        LoadData();
     }
 
-    private void LoadData()
+    private bool LoadNextData()
     {
-        _db.QueryCount(ref _bookData, _startCallNum, _capacity, false);
-        if (_bookData.Count == 0)
-            throw new IndexOutOfRangeException("Database query returned 0 entries");
+        var initCount = _buffer.Count;
+        _db.QueryCount(ref _buffer, _startCallNum, _capacity,
+            startInclusive: false, forward: true);
+        return _buffer.Count > initCount; // check if we added data
     }
-
-    private Book NextBook()
+    
+    [CanBeNull]
+    private DataEntry NextEntry()
     {
-        if (_bookData.Count == 0) LoadData();
-        
-        // load more data if needed
-        if (_index >= _bookData.Count)
+        if (_buffer.Count == 0 && !LoadNextData()) return null;
+
+        // load next round of data if needed
+        if (_index >= _buffer.Count)
         {
-            _startCallNum = _bookData[_bookData.Count - 1].CallNum;
-            LoadData();
+            _startCallNum = _buffer[_buffer.Count - 1].CallNum;
+            if (!LoadNextData()) return null;
         }
-        
-        
+
+        return _buffer[_index++];
     }
 }
