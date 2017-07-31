9db1d3067dda4d2a8766736085da4d1fe77fe40d
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-07-31 12:03:22 -0700

    0.2.6 Add timer debug class

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+public class Timer
+{
+    private readonly Stopwatch _watch = new Stopwatch();
+
+    public Timer()
+    {
+        Start();
+    }
+
+    public void Start()
+    {
+        _watch.Start();
+    }
+        
+    public void Stop(string msg)
+    {
+        _watch.Stop();
+        Debug.Log(msg + ": " + _watch.Elapsed.TotalMilliseconds);
+    }
+}
