3cf4dc492ca88959ba88651abfd4e82d3022ad57
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-08-18 17:12:56 -0700

    0.1.0 Extend direction enum to convert to vector

--- Assets/Scripts/Direction.cs
+++ Assets/Scripts/Direction.cs
@@ -1,6 +1,27 @@
+using System;
+using UnityEngine;
+
 public enum Direction
 {
     Right,
     Left,
     Identity
+}
+
+static class DirectionVectorExtension
+{
+    public static Vector3 ToVector(this Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Right:
+                return Vector3.right;
+            case Direction.Left:
+                return Vector3.left;
+            case Direction.Identity:
+                throw new ArgumentOutOfRangeException("direction", direction, null);
+            default:
+                throw new ArgumentOutOfRangeException("direction", direction, null);
+        }
+    }
 }
