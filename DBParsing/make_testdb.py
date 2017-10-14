bf64083891c635a81af95cb401660fb9334640fe
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-10-14 16:42:25 -0700

    Write script to generate testing db

---
+++ DBParsing/make_testdb.py
@@ -0,0 +1,43 @@
+import sqlite3
+import random
+
+### User Values
+db_file = 'testdb.db'       # data base file path
+table = 'testing'           # name of table to populate
+call_index = 'call_index'   # name for look up index
+call_col = 'call'           # col to index for look up
+size = 100000
+###
+
+insertq = 'INSERT INTO testing VALUES (?, ?, ?)'
+dropq = 'DROP INDEX IF EXISTS ' + call_index
+indexq = 'CREATE INDEX ' + call_index + ' ON ' + table + '(' + call_col + ')'
+deleteq = 'DELETE FROM testing'
+
+alphabet = []
+alphabet.extend("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+conn = sqlite3.connect(db_file)
+curs = conn.cursor()
+curs.execute(deleteq)
+curs.execute(dropq)
+curs.execute(indexq)
+
+subCount = int(size / len(alphabet))
+for topLetter in alphabet:
+    for i in range(0, subCount):
+        botLetter = random.choice(alphabet)
+        num = str(random.randrange(0, 10000))
+        subLetter = random.choice(alphabet)
+        subNum = str(random.randrange(0, 100))
+
+        call = topLetter + botLetter + num + ' .' + subLetter + subNum
+        title = 'Sample'
+        width = 20 + random.randrange(-10, 10)
+
+        print(call, title, width)
+        curs.execute(insertq, (call, title, width))
+
+conn.commit()
+conn.close()
+
