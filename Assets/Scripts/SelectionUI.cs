d0949d6bd6c8fa9a4db50c07c939812035b4b494
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-10-21 13:46:01 -0700

    0.4.1 Establish communication skeleton between slider and main UI

--- Assets/Scripts/SelectionUI.cs
+++ Assets/Scripts/SelectionUI.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class SelectionUI : MonoBehaviour
 {
 	public Text Title;
 	public Slider Slider;
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		// TODO
+	}
+	
+	public void OnSliderValueChanged(float value)
+	{
+		Debug.Log(value);
+	}
 }
