8bea98eaa5652d08c84382a2264858b2ee09b3ec
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-10-21 14:25:49 -0700

    0.4.1 Convert slider value to alphabet and display

--- Assets/Scripts/SelectionUI.cs
+++ Assets/Scripts/SelectionUI.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using Oculus.Platform;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class SelectionUI : MonoBehaviour
 {
+	private string[] alphabet = new[]
+	{
+		"A", "B", "C", "D", "E", "F", "G",
+		"H", "I", "J", "K", "L", "M", "N",
+		"O", "P", "Q", "R", "S", "T", "U",
+		"V", "W", "X", "Y", "Z"
+	};
+	
 	public Text Title;
 	public Slider Slider;
 
@@ -14,6 +25,12 @@ public class SelectionUI : MonoBehaviour
 	
 	public void OnSliderValueChanged(float value)
 	{
-		Debug.Log(value);
+		Title.text = GetSliderTranslation(value);
+	}
+	
+	private string GetSliderTranslation(float value)
+	{
+		var index = (int) Math.Floor(value * 27);
+		return alphabet[index];
 	}
 }
