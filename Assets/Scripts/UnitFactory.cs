f82b4c960e7d9c64e79ff16aade4e949b4523407
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-10-31 18:07:10 -0700

    0.4.1 Unit Factory uses only one reference

--- Assets/Scripts/UnitFactory.cs
+++ Assets/Scripts/UnitFactory.cs
@@ -5,17 +5,23 @@ public class UnitFactory : MonoBehaviour
 {
     public BookshelfController TemplateShelf;
 
-    public Unit UnitStartCallNum(string start)
+    private BookshelfController bookshelf;
+    private Unit unit;
+
+    private void Start()
     {
-        var bookshelf = Instantiate(TemplateShelf, gameObject.transform);
-        bookshelf.transform.position += Vector3.down * 100; // move from visible space
-        bookshelf.SetStartEndCallNum(start, "");
-        var unit = bookshelf.GetComponent<Unit>();
+        bookshelf = Instantiate(TemplateShelf, gameObject.transform);
+        bookshelf.transform.position += Vector3.down * 100;
+        unit = bookshelf.GetComponent<Unit>();
         if (unit == null)
         {
             Debug.LogError("UnitFactory could not find Unit component on TemplateShelf");
-            return null;
         }
+    }
+
+    public Unit UnitStartCallNum(string start)
+    {
+        bookshelf.SetStartEndCallNum(start, "");
         return unit;
     }
 }
