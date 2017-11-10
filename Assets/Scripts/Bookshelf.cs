4fb6a75e916fb5f4274c9849f5d86bd7b6a74d4d
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-11-09 18:21:49 -0800

    0.4.3 Add Bookshelf class

---
+++ Assets/Scripts/Bookshelf.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public class Bookshelf
+{
+    public string Start { get; private set; }
+    public string End { get; private set; }
+    public LinkedList<LinkedList<Book>> Table { get; private set; }
+}
