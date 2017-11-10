1ce469531cfa1a1454ad54e60f57458fdd9e7ea5
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-11-09 19:00:31 -0800

    0.4.3 Add bookshelf adding delegates

--- Assets/Scripts/Bookshelf.cs
+++ Assets/Scripts/Bookshelf.cs
@@ -5,4 +5,25 @@ public class Bookshelf
     public string Start { get; private set; }
     public string End { get; private set; }
     public LinkedList<LinkedList<Book>> Table { get; private set; }
+
+    private delegate void ShelfAdder(LinkedList<Book> books, Book book);
+
+    private delegate void TableAdder(LinkedList<LinkedList<Book>> table, LinkedList<Book> shelf);
+
+    private ShelfAdder addShelf;
+    private TableAdder addTable;
+
+    public void Load(Direction direction)
+    {
+    }
+
+    private void AddRight<T>(LinkedList<T> list, T item)
+    {
+        list.AddLast(item);
+    }
+
+    private void AddLeft<T>(LinkedList<T> list, T item)
+    {
+        list.AddFirst(item);
+    }
 }
