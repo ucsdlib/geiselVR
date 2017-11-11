d66f569225f026b4e275daa3eb46578dc719ca0f
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-11-11 15:30:26 -0800

    Add Manager to manage global settings

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
