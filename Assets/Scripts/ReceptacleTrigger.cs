30279ec14686ff02617ba7816703c2246ad543c3
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-12-02 15:19:06 -0800

    0.5.0 Books trigger receptacle

---
+++ Assets/Scripts/ReceptacleTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceptacleTrigger : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log ("Triggered");
+
+		var bookController = other.GetComponentInParent<BookController> ();
+		if (bookController == null) {
+			Debug.Log ("Could not find controller");
+			return;
+		}
+
+		Debug.Log ("Found Book");
+	}
+}
