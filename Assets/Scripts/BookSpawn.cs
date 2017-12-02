0d596892d02788d4a239abd7434f6c90799c76f5
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-12-02 14:47:19 -0800

    0.5.0 Place and load sample books

--- Assets/Scripts/BookSpawn.cs
+++ Assets/Scripts/BookSpawn.cs
@@ -1,22 +1,19 @@
-﻿﻿using UnityEngine;
+﻿using UnityEngine;
 
 public class BookSpawn : MonoBehaviour
 {
-    [SerializeField] private Transform _book;
+	public BookController book1;
+	public BookController book2;
+	public BookController book3;
 
-    private void Start()
-    {
-        Debug.Log("Started BookSpawn Script");
-    }
+	private void Start()
+	{
+		book1.SetMeta (new Book ("A1", "Adventures of Finn", 20.0, "Thomas Paine", "A book about adventure"));
+		book2.SetMeta (new Book ("A2", "The Game", 19.0, "Giovanni Grio", "Witness the epic tale of a yonug boy battling for the grand prize"));
+		book3.SetMeta (new Book ("A3", "Star Wars", 21.0, "George Lucas", "In a galaxy far far away, there are Jedi with lightsabers"));
 
-    private void Update()
-    {
-        // Check if the user pressed the button
-        if (OVRInput.GetDown(OVRInput.Button.One))
-        {
-            Debug.Log("Pressed 'A' Button");
-            Instantiate(_book, gameObject.transform.position,
-                gameObject.transform.rotation);
-        }
-    }
+		book1.LoadData ();
+		book2.LoadData ();
+		book3.LoadData ();
+	}
 }
