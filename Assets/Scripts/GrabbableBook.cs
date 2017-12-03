b02d693b39c2cd46d751e1230ed2f06558b8869b
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-12-02 16:21:04 -0800

    0.5.0 Inherit from OVR classes to allow custom behavior

---
+++ Assets/Scripts/GrabbableBook.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableBook : OVRGrabbable
+{
+    public void ForceEnd()
+    {
+        if (!isGrabbed) return;
+        var modGrabber = m_grabbedBy as ModGrabber;
+        if (modGrabber == null)
+        {
+            Debug.LogError("Book needs ModGrabber to force end");
+            return;
+        }
+        modGrabber.ForceRelease();
+    }
+}
---
+++ Assets/Scripts/ModGrabber.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModGrabber : OVRGrabber {
+    public void ForceRelease()
+    {
+        GrabEnd();
+    }
+}
