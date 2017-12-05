e49336604cf97e1cc30835b019cb3771c533bc83
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-12-05 14:50:03 -0800

    0.5.0 Receptacle admits only one book at a time

--- Assets/Scripts/ReceptacleTrigger.cs
+++ Assets/Scripts/ReceptacleTrigger.cs
@@ -7,8 +7,12 @@ public class ReceptacleTrigger : MonoBehaviour
     public DataUI DataUi;
     public Transform SnapPoint;
 
+    private Collider obj = null;
+    
+
     private void OnTriggerEnter(Collider other)
     {
+        if (obj != null) return;
         var bookController = other.GetComponent<BookController>();
         if (bookController == null)
         {
@@ -21,5 +25,15 @@ public class ReceptacleTrigger : MonoBehaviour
         bookController.transform.parent = transform;
         bookController.transform.position = SnapPoint.position;
         bookController.transform.rotation = SnapPoint.rotation;
+
+        obj = other;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == obj)
+        {
+            obj = null;
+        }
     }
 }
