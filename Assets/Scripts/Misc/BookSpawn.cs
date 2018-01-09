46e5280cb27000148e7a6c34ce3108cc48ffa639
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-01-09 13:44:36 -0800

    Add canvas testing scene and relevat scripts

---
+++ Assets/Scripts/Misc/BookSpawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BookSpawn : MonoBehaviour
+{
+	public BookController book1;
+	public BookController book2;
+	public BookController book3;
+
+	private void Start()
+	{
+		book1.SetMeta (new Book ("A1", "Adventures of Finn, and something to make title longer", 20.0, "Thomas Paine", "A book about adventure, and something to make the title a lot longer"));
+		book2.SetMeta (new Book ("A2", "The Game of the Seven Hundred Countries and we can put so much more on this title", 19.0, "Giovanni Grio", "Witness the epic tale of a yonug boy battling for the grand prize"));
+		book3.SetMeta (new Book ("A3", "Star Wars The Force Awakens and the story of Harry Potter", 21.0, "George Lucas", "In a galaxy far far away, there are Jedi with lightsabers"));
+
+		book1.LoadData ();
+		book2.LoadData ();
+		book3.LoadData ();
+	}
+}
---
+++ Assets/Scripts/Misc/CanvasSpawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasSpawn : MonoBehaviour
+{
+    public Canvas CanvasTemplate;
+    public int GridSize;
+    private const float Spacing = 0.5f;
+
+    private List<Canvas> all = new List<Canvas>();
+    private int lastGridSize;
+
+    private void Start()
+    {
+        lastGridSize = GridSize;
+        GenerateGrid(GridSize, Spacing);
+    }
+
+    private void Update()
+    {
+        if (GridSize == lastGridSize) return;
+            
+        lastGridSize = GridSize;
+        foreach (var canvas in all)
+        {
+            Destroy(canvas);
+        }
+        all.Clear();
+        GenerateGrid(GridSize, Spacing);
+    }
+
+    private void GenerateGrid(int gridSize, float spacing)
+    {
+        for (var i = 0; i < gridSize; i++)
+        {
+            var offset = i * spacing * Vector3.right;
+            for (var j = 0; j < gridSize; j++)
+            {
+                var position = offset + j * spacing * Vector3.forward;
+                var canvas = Instantiate(CanvasTemplate);
+                canvas.transform.position = position;
+                all.Add(canvas);
+            }
+        }
+    }
+}
