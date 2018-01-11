e8778df7bfe0367bf935441bedce907d961602b5
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-01-11 15:31:07 -0800

    0.5.2 Scroll spline text

--- Assets/Scripts/BookDisplayController.cs
+++ Assets/Scripts/BookDisplayController.cs
@@ -1,15 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BookDisplayController : MonoBehaviour
 {
-    public float ScrollSpeed;
-    /*
-     * Assumption is made that text is anchored and positioned in
-     * in its left most extreme. Text is scrolled to the left
-     */
+    public float ScrollTimeFactor;
     public Text SpineText;
 
     private volatile bool lerping;
@@ -17,11 +15,29 @@ public class BookDisplayController : MonoBehaviour
     private void Update()
     {
         // scroll if text exceeds bounds
-        if (SpineText.preferredWidth > SpineText.rectTransform.rect.width)
+        if (!lerping && SpineText.preferredWidth > SpineText.rectTransform.rect.width)
         {
-            
+            lerping = true;
+            var distance = SpineText.preferredWidth - SpineText.rectTransform.rect.width;
+            StartCoroutine(ShiftSpine(ScrollTimeFactor, distance));
         }
+        SpineText.rectTransform.anchoredPosition += Vector2.left * 0.1f;
     }
     
-    
+    private IEnumerator ShiftSpine(float timeFactor, float distance)
+    {
+        var start = SpineText.rectTransform.anchoredPosition;
+        var end = SpineText.rectTransform.anchoredPosition + distance * Vector2.left;
+        var t = 0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime / timeFactor;
+            SpineText.rectTransform.anchoredPosition = Vector2.Lerp(start, end, t);
+            yield return null;
+        }
+
+        // TODO make a smoother transition back to start
+        SpineText.rectTransform.anchoredPosition = start;
+        lerping = false;
+    }
 }
