8dfc2ffb5fbf6d41c0b25a10c53cca4f20860e19
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-01-11 14:48:45 -0800

    0.5.2 Arrange pivots and anchors for book spine text. Add BookDisplayController

---
+++ Assets/Scripts/BookDisplayController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookDisplayController : MonoBehaviour
+{
+    public float ScrollSpeed;
+    /*
+     * Assumption is made that text is anchored and positioned in
+     * in its left most extreme. Text is scrolled to the left
+     */
+    public Text SpineText;
+
+    private volatile bool lerping;
+
+    private void Update()
+    {
+        // scroll if text exceeds bounds
+        if (SpineText.preferredWidth > SpineText.rectTransform.rect.width)
+        {
+            
+        }
+    }
+    
+    
+}
