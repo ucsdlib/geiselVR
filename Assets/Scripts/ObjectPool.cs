6e2f5c3d2de7bc17559ba9831b5bb53bc5fd4ec4
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-01-22 15:16:54 -0800

    0.6.0 Add ObjectPool

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+    public GameObject Template; // note that OnEnable, OnDisable should be implemented
+    public int StartSize;
+
+    private readonly List<GameObject> pool = new List<GameObject>();
+
+    private void Start()
+    {
+        for (var i = 0; i < StartSize; i++)
+        {
+            var o = Instantiate(Template);
+            o.SetActive(false);
+            pool.Add(o);
+        }
+    }
+    
+    public GameObject Borrow()
+    {
+        foreach (var o in pool)
+        {
+            if (o.activeInHierarchy) continue;
+            o.SetActive(true);
+            return o;
+        }
+        var newo = Instantiate(Template);
+        pool.Add(newo);
+        return newo;
+    }
+    
+    public void GiveBack(GameObject o)
+    {
+        o.SetActive(false);
+    }
+}
