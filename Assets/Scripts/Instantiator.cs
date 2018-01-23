e7f0837e800e5c308d3d7d1051c9bb3ae38b7b76
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-01-23 12:49:04 -0800

    0.6.0 Add Instantiator

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject Template;
+
+    public List<GameObject> InstantiateGroup(int size)
+    {
+        var group = new List<GameObject>();
+
+        for (var i = 0; i < size; i++)
+        {
+            var o = Instantiate(Template);
+            Template.SetActive(false);
+            group.Add(o);
+        }
+
+        return group;
+    }
+
+    public void DestroyGroup(IEnumerable<GameObject> objs)
+    {
+        foreach (var o in objs)
+        {
+            Destroy(o);
+        }
+    }
+}
