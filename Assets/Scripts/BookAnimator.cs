7ca0c325dadced5a357ccc234e6ec08fc9649813
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-02-06 14:24:31 -0800

    0.5.2 Add BookAnimator

---
+++ Assets/Scripts/BookAnimator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BookAnimator : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger enter"); // DEBUG
+        var bookController = other.GetComponent<BookController>();
+        if (bookController == null)
+        {
+            Debug.Log("Could not find book controller"); // DEBUG
+            return;
+        }
+
+        Debug.Log("Changing animate"); // DEBUG
+        bookController.Display.Animate = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var bookController = other.GetComponent<BookController>();
+        if (bookController == null)
+        {
+            Debug.Log("Could not find book controller"); // DEBUG
+            return;
+        }
+
+        Debug.Log("Changing animate"); // DEBUG
+        bookController.Display.Animate = false;
+    }
+}
