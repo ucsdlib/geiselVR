4cf77cb6385a33b5525ce0f5faf17419f0faf82e
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-02-08 16:31:55 -0800

    0.5.3 Books randomly colored at start up

--- Assets/Scripts/BookController.cs
+++ Assets/Scripts/BookController.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
+using Random = System.Random;
 
 public class BookController : MonoBehaviour
 {
-
     public BookDisplayController Display;
 
     public float Width
@@ -11,6 +11,17 @@ public class BookController : MonoBehaviour
     }
 
 	public Book Meta { get; set; }
+    
+    private static Random random = new Random();
+
+    private void Awake()
+    {
+        // set random material
+        var materials = Manager.Instance.BookMaterials;
+        if (materials.Count == 0) return;
+        var index = random.Next(materials.Count);
+        gameObject.GetComponent<Renderer>().material = materials[index];
+    }
 
     public void SetMeta(Book meta)
     {
