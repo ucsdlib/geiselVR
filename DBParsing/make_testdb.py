1d3f2432a28a6ea5399ee7a4277bf7adb7575714
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2018-02-15 23:53:37 -0800

    Conform make_testdb to coldata

--- DBParsing/make_testdb.py
+++ DBParsing/make_testdb.py
@@ -6,9 +6,10 @@ database will be compatible with the GeiselVR program.
 """
 import random
 import sqlite3
+import util
+import coldata
 
-# User Values
-
+### User Values
 # Output database file path
 db_file = 'testdb.db'
 # Desired SQLite table name
@@ -21,26 +22,22 @@ call_col = 'call'
 size = 100000
 # Title of every book in database
 def_title = 'Sample Book Sample Book Sample Book'
-
-#
-
-tableq = 'CREATE TABLE IF NOT EXISTS ' + table + '(' + call_col + ' text PRIMARY KEY, title text, width real);'
-insertq = 'INSERT INTO testing VALUES (?, ?, ?)'
-dropq = 'DROP INDEX IF EXISTS ' + call_index
-indexq = 'CREATE INDEX ' + call_index + ' ON ' + table + '(' + call_col + ')'
-deleteq = 'DELETE FROM testing'
+# Padding for values not generated
+padding = "N/A"
+###
 
 alphabet = []
 alphabet.extend("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 conn = sqlite3.connect(db_file)
 curs = conn.cursor()
-curs.execute(tableq)
-curs.execute(deleteq)
-curs.execute(dropq)
-curs.execute(indexq)
+curs.execute(util.query_delete_table(table))
+curs.execute(util.query_create_table(table, coldata.ids, coldata.names))
+curs.execute(util.query_drop_index(call_index))
+curs.execute(util.query_create_index(table, call_index, call_col))
 
 subCount = int(size / len(alphabet))
+unique_id = 0
 for topLetter in alphabet:
     for i in range(0, subCount):
         botLetter = random.choice(alphabet)
@@ -51,9 +48,20 @@ for topLetter in alphabet:
         call = topLetter + botLetter + num + ' .' + subLetter + subNum
         title = def_title
         width = 20 + random.randrange(-10, 10)
+        # TODO what if these are not included in coldata?
 
-        print(call, title, width)
-        curs.execute(insertq, (call, title, width))
+        # pad insertion to appropiate size
+        data = [padding] * len(coldata.ids)
+        data[coldata.table[util.ITEM_NUM]] = unique_id
+        data[coldata.table[util.CALL_NUM]] = call
+        data[coldata.table[util.TITLE]] = title
+        data[coldata.table[util.DIM_OR_DESC]] = width
+
+        curs.execute(
+            util.query_insert(table, len(coldata.ids)),
+            data
+        )
+        unique_id += 1
 
 conn.commit()
 conn.close()
