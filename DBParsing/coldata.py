bd7a9b9c4255d9cfc739b1b25032c69c647fceff
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2018-02-15 22:18:17 -0800

    Add module to define required columns more easily

---
+++ DBParsing/coldata.py
@@ -0,0 +1,26 @@
+"""
+Defines the columns which GeiselVR is currently expecting.
+"""
+import util
+
+colids = [
+    util.ISBN,
+    util.CALL_NUM,
+    util.TITLE,
+    util.AUTHOR,
+    util.DIM_OR_DESC,
+    util.GENRE,
+    util.SUBJECT,
+    util.SUMMARY
+]
+
+colnames = [
+    'id',
+    'call',
+    'title',
+    'author',
+    'width',
+    'genre',
+    'subject',
+    'summary'
+]
