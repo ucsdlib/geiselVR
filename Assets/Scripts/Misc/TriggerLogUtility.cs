b15b8c296a45285f38a4616dfa13ece355281a6f
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-02-20 13:12:21 -0800

    Add TriggerLogUtility

---
+++ Assets/Scripts/Misc/TriggerLogUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLogUtility : MonoBehaviour
+{
+    public string Tag = "TriggerLog";
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(Tag + ": trigger enter");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(Tag + ": trigger exit");
+    }
+}
