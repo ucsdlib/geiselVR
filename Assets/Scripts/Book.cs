81eab2fe62fca36d8b29efc81406eb1e61049d82
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-02-22 12:42:20 -0800

    Document Book

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -1,21 +1,32 @@
-﻿public class Book
+﻿/// <summary>
+/// A model class containing book data. Can be used to populate <see cref="BookController"/>.
+/// </summary>
+public class Book
 {
+    // Adjusts spacing on bookshelf. Higher values increase spacing.
     private const float WidthMult = 2.0f;
 
+    // Data getters
     public string Id { get; private set; }
     public string CallNumber { get; private set; }
     public string Title { get; private set; }
     public string Author { get; private set; }
+
     public float Width
     {
         get { return (float) widthDb * WidthMult / 1000 + 0.01f; }
     }
+
     public string Genre { get; private set; }
     public string Subject { get; private set; }
     public string Summary { get; private set; }
 
     private readonly double widthDb;
 
+    /// <summary>
+    /// Constructs from a database entry
+    /// </summary>
+    /// <param name="entry">A database entry typically obtained from <see cref="DbBuffer"/></param>
     public Book(DataEntry entry)
     {
         Id = entry.Id;
@@ -28,6 +39,9 @@
         Summary = entry.Summary;
     }
 
+    /// <summary>
+    /// Constructs with provided data. Used for testing
+    /// </summary>
     public Book(
         string id, string callNumber, string title, string author,
         double width, string genre, string subject, string summary)
