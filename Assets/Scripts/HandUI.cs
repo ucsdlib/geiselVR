1d85381cb10140ecb2e8d2e3930e27a15a6d00cb
Jean-Luc Thumm <jthumm@ucsd.edu>
2018-03-21 16:17:52 -0700

    0.8.1 Implement Hand UI toggle

---
+++ Assets/Scripts/HandUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+
+public class HandUI : MonoBehaviour
+{
+    public GameObject HandUIObj;
+
+    private float lastPressTime = 0;
+    
+    private void Update()
+    {
+        if(OVRInput.Get(OVRInput.Button.One) && Time.time - lastPressTime > 0.5)
+        {
+            lastPressTime = Time.time;
+            if (HandUIObj.activeInHierarchy)
+            {
+                HandUIObj.SetActive(false);
+            }
+            else
+            {
+                HandUIObj.SetActive(true);
+            }
+        }
+    }
+}
